c6142a22a7116e7cb04ac4c32c65c65849086e87
RadinTiholov <radin.tiholov@mail.bg>
2022-06-20 22:29:20 +0300

    Fixed the layout if index.js

--- Exam prep 1 (Art gallery app)/src/index.js
+++ Exam prep 1 (Art gallery app)/src/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const hbs = require('express-handlebars');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+
 const {PORT} = require('./config/env.js');
 const routes = require('./routes');
-const path = require('path');
 const dbInit = require('./config/initDb.js')
-const cookieParser = require('cookie-parser');
 const {auth} = require('./middlewares/authMiddleware');
 const { errorHandler } = require('./middlewares/errorHandlerMiddleware.js');
 
@@ -17,9 +18,7 @@ app.use(cookieParser());
 app.use(auth);
 app.use(routes);
 app.use(errorHandler)
-app.get('*', (req, res) => {
-    res.status(404).render('404');
-});
+
 
 //Init the hbs
 app.engine('hbs', hbs.engine({
