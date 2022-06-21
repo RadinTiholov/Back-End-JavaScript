9d22f43f66444a9e384b6334a3b934f478eab2ae
RadinTiholov <radin.tiholov@mail.bg>
2022-06-21 22:15:53 +0300

    Added a comment

--- Exam prep 1 (Art gallery app)/src/services/publicationService.js
+++ Exam prep 1 (Art gallery app)/src/services/publicationService.js
@@ -6,4 +6,4 @@ exports.getAllLeaned = () => Publication.find().lean();
 exports.getOneDetailed = (userId) => Publication.findById(userId).populate('author').lean();
 exports.getOne = (userId) => Publication.findById(userId);
 exports.delete = (_id) => Publication.deleteOne({_id});
-exports.update = (_id, data) => Publication.findByIdAndUpdate(_id, data);
+exports.update = (_id, data) => Publication.findByIdAndUpdate(_id, data);//,{runValidators: true});
