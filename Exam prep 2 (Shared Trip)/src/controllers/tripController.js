49d2c1ebcc8d34f8f55c97e4c50d81a77fcb06be
RadinTiholov <radin.tiholov@mail.bg>
2022-06-22 17:33:45 +0300

    Created the details page

--- Exam prep 2 (Shared Trip)/src/controllers/tripController.js
+++ Exam prep 2 (Shared Trip)/src/controllers/tripController.js
@@ -11,11 +11,25 @@ router.get('/offer', isAuth, (req, res) => {
 router.post('/offer', isAuth, async (req, res) => {
     console.log(req.body);
     try{
-        const createdtrip = await tripService.create(req.body);
-        res.redirect('/shared');
+        const author = req.user._id;
+        const createdtrip = await tripService.create({...req.body, author});
+        res.redirect('/trip/shared');
     }catch(error){
         res.render('trip/create', {error: error.message});
     }
+})
+
+router.get('/shared', async (req, res) => {
+    const trips = await tripService.getAll().lean();
+    res.render('trip/shared', {trips});
+})
+
+router.get('/details/:id', async (req, res) => {
+    const trip = await tripService.getOneDetailed(req.params.id).lean();
+    const author = await tripService.getAuthor(trip.author).lean();
+    const tripRaw = await tripService.getOne(req.params.id);
+    
+    res.render('trip/details', {...trip, authorObj:author});
 })
 
 module.exports = router;
--- Exam prep 2 (Shared Trip)/src/services/tripService.js
+++ Exam prep 2 (Shared Trip)/src/services/tripService.js
@@ -1,3 +1,8 @@
 const Trip = require('../models/Trip');
+const User = require('../models/User');
 
 exports.create = (data) => Trip.create(data);
+exports.getAll = () => Trip.find();
+exports.getOne = (id) => Trip.findById(id); 
+exports.getOneDetailed = (id) => Trip.findById(id).populate('buddies'); 
+exports.getAuthor = (id) => User.findById(id);
