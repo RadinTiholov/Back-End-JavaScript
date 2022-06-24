7e40076c05bc8c4964254a5fc388aa92e1210987
RadinTiholov <radin.tiholov@mail.bg>
2022-06-24 19:53:19 +0300

    Fixed the home page

--- Exam prep 3 (Real Estate Agency)/src/controllers/homeController.js
+++ Exam prep 3 (Real Estate Agency)/src/controllers/homeController.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
+const housingService = require('../services/housingService.js');
 
-router.get('/', (req, res) => {
-    res.render('home/index');
+router.get('/', async (req, res) => {
+    const housingsGot = await housingService.getAll().lean();
+    const housings = housingsGot.slice(0, 3);
+    res.render('home/index', {housings});
 })
 
 module.exports = router;
