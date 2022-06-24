b3516720b6b48e0fa8441523877818be4f9bf55c
RadinTiholov <radin.tiholov@mail.bg>
2022-06-24 19:47:29 +0300

    Added details, rent housing and delete functionality

--- Exam prep 3 (Real Estate Agency)/src/controllers/housingController.js
+++ Exam prep 3 (Real Estate Agency)/src/controllers/housingController.js
@@ -31,4 +31,49 @@ router.post('/create', isAuth, async (req, res) => {
     }
 })
 
+router.get('/details/:id', async (req,res) => {
+    const housing = await housingService.getOneDetailed(req.params.id).lean();
+    const housingRaw = await housingService.getOne(req.params.id);
+    let rentedAHouseJoined = null;
+    const isAuthor = req.user?._id == housing.owner._id;
+    const isJoined = housingRaw.renredAHome.includes(req.user?._id);
+    const isAvailable = housing.availablePieces - housing.renredAHome.length > 0;
+    const availablePlaces = housing.availablePieces - housing.renredAHome.length;
+
+    rentedAHouseJoined = housing.renredAHome.map(x => x.name).join(', ');
+    console.log(rentedAHouseJoined);
+
+    res.render('housing/details', {...housing, rentedAHouseJoined,isAuthor, isJoined, isAvailable, availablePlaces});
+})
+
+router.get('/rent/:id', isAuth, async (req, res) => {
+    const housing = await housingService.getOneDetailed(req.params.id).lean();
+    const housingRaw = await housingService.getOne(req.params.id);
+    const isAuthor = req.user?._id == housing.owner._id;
+    const isAvailable = housing.availablePieces - housing.renredAHome.length > 0;
+    const isJoined = housingRaw.renredAHome.includes(req.user?._id);
+    if(!isAuthor && !isJoined && isAvailable){
+        housingRaw.renredAHome.push(req.user._id)
+        housingRaw.save();
+
+        res.redirect(`/housing/details/${req.params.id}`)
+    }
+    else{
+        res.render('404', {error: "Unauthorized to do this action."})
+    }
+})
+
+router.get('/delete/:id', isAuth, async (req, res) => {
+    const housing  = await housingService.getOneDetailed(req.params.id).lean();
+    const isAuthor = req.user._id == housing.owner._id;
+    if(isAuthor){
+        await housingService.delete(req.params.id);
+
+        res.redirect('/housing/all');
+    }
+    else{
+        res.status(401).render('404', {error: "Unauthorized to do this action."})
+    }
+})
+
 module.exports = router;
--- Exam prep 3 (Real Estate Agency)/src/services/housingService.js
+++ Exam prep 3 (Real Estate Agency)/src/services/housingService.js
@@ -2,3 +2,6 @@ const Housing = require('../models/Housing');
 
 exports.create = (data) => Housing.create(data);
 exports.getAll = () => Housing.find();
+exports.getOneDetailed = (id) => Housing.findById(id).populate('renredAHome');
+exports.getOne = (id) => Housing.findById(id);
+exports.delete = (_id) => Housing.deleteOne({_id});
