7baef8967e5723430628676d5c14eb71424fef27
RadinTiholov <radin.tiholov@mail.bg>
2022-06-24 18:47:39 +0300

    Wrote search functionality

--- Exam prep 3 (Real Estate Agency)/src/controllers/housingController.js
+++ Exam prep 3 (Real Estate Agency)/src/controllers/housingController.js
@@ -3,11 +3,22 @@ const housingService = require('../services/housingService.js');
 
 const router = require('express').Router();
 
-router.get('/all', (req, res) => {
-    res.render('housing/all');
+router.get('/all', async (req, res) => {
+    const housings = await housingService.getAll().lean();
+    res.render('housing/all', {housings});
 })
 
-router.get('/create', isAuth, async (req, res) => {
+router.get('/search', isAuth, (req, res) => {
+    res.render('housing/search' , {firstEnter: true});
+})
+
+router.post('/search', isAuth, async (req, res) => {
+    const search = req.body.search;
+    const housings = await (await housingService.getAll().lean()).filter(x => x.type == search);
+    res.render('housing/search' , {housings, firstEnter: false});
+})
+
+router.get('/create', isAuth, (req, res) => {
     res.render('housing/create')
 })
 router.post('/create', isAuth, async (req, res) => {
--- Exam prep 3 (Real Estate Agency)/src/services/housingService.js
+++ Exam prep 3 (Real Estate Agency)/src/services/housingService.js
@@ -1,3 +1,4 @@
 const Housing = require('../models/Housing');
 
 exports.create = (data) => Housing.create(data);
+exports.getAll = () => Housing.find();
