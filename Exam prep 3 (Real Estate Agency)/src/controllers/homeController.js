3ecd192030315c5367c6a0f3610647e011d4eb75
RadinTiholov <radin.tiholov@mail.bg>
2022-06-24 20:09:06 +0300

    Fixed bug in the search functionality

--- Exam prep 3 (Real Estate Agency)/src/controllers/homeController.js
+++ Exam prep 3 (Real Estate Agency)/src/controllers/homeController.js
@@ -3,7 +3,7 @@ const housingService = require('../services/housingService.js');
 
 router.get('/', async (req, res) => {
     const housingsGot = await housingService.getAll().lean();
-    const housings = housingsGot.slice(0, 3);
+    const housings = housingsGot.reverse().slice(0, 3);
     res.render('home/index', {housings});
 })
 
