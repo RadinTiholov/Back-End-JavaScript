d4059bbfaf89bc920ba337c7e43c156a5a10b944
RadinTiholov <radin.tiholov@mail.bg>
2022-06-26 16:54:47 +0300

    Wrote details and update actions

--- Workshop REST API/server/src/controllers/furnitureController.js
+++ Workshop REST API/server/src/controllers/furnitureController.js
@@ -13,5 +13,31 @@ router.post('/', async (req, res) => {
         res.status(404).json({message: "Request error"});
     }
 })
+router.get('/:id', async (req, res) => {
+    try{
+        const furniture = await furnitureService.getOne(req.params.id).lean();
+        res.json(furniture);
+    }catch(error){
+        res.status(404).json({message: "Request error"});
+    }
+})
+router.put('/:id', async (req, res) => {
+    try{
+        const data = req.body;
+        const furniture = await furnitureService.update(req.params.id, data);
+        res.json(furniture);
+    }catch(error){
+        console.log(error);
+        res.status(404).json({message: "Request error"});
+    }
+})
+router.delete('/:id', async (req, res) => {
+    try{
+        const furniture = await furnitureService.delete(req.params.id);
+        res.json(furniture);
+    }catch(error){
+        res.status(404).json({message: "Request error"});
+    }
+})
 
 module.exports = router;
--- Workshop REST API/server/src/services/furnitureService.js
+++ Workshop REST API/server/src/services/furnitureService.js
@@ -2,3 +2,6 @@ const Furniture = require('../models/Furniture.js');
 
 exports.getAll = () => Furniture.find();
 exports.create = (data) => Furniture.create(data);
+exports.getOne = (id) => Furniture.findById(id);
+exports.update = (id, data) => Furniture.findByIdAndUpdate(id, data);
+exports.delete = (_id) => Furniture.deleteOne({_id});
