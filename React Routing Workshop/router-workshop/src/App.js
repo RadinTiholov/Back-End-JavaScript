8cfc2b0833d03771a26e56f75f1bd61b83c8d308
RadinTiholov <radin.tiholov@mail.bg>
2022-07-28 21:29:15 +0300

    Lift games state

--- React Routing Workshop/router-workshop/src/App.js
+++ React Routing Workshop/router-workshop/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import {
-  Routes,
-  Route
+    Routes,
+    Route
 } from "react-router-dom";
+import { AuthContext } from './contexts/AuthContext';
+import { useState, useEffect } from 'react';
 
 import { Header } from './components/Header/Header';
 import { Home } from './components/Home/Home';
@@ -13,26 +15,36 @@ import { Edit } from './components/Edit/Edit';
 import { Details } from './components/Details/Details';
 import { Catalogue } from './components/Catalogue/Catalogue';
 import { NotFound } from './components/NotFound/NotFound';
+import * as gamesService from "./services/gamesService";
 
 function App() {
-  return (
-    <div id="box">
-      <Header />
-      {/* Main Content */}
-      <main id="main-content"></main>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/create' element={<Create />} />
-        <Route path='/edit/:id' element={<Edit />} />
-        <Route path='/details/:id' element={<Details />} />
-        <Route path='/catalogue' element={<Catalogue />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-    </div>
+    
+    const [games, setGames] = useState([]);
 
-  );
+    useEffect(() => {
+        gamesService.getAll()
+            .then(res => setGames(res));
+    }, [])
+    
+    return (
+        <AuthContext.Provider>
+            <div id="box">
+                <Header />
+                {/* Main Content */}
+                <main id="main-content"></main>
+                <Routes>
+                    <Route index element={<Home />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/register' element={<Register />} />
+                    <Route path='/create' element={<Create />} />
+                    <Route path='/edit/:id' element={<Edit />} />
+                    <Route path='/details/:id' element={<Details />} />
+                    <Route path='/catalogue' element={<Catalogue games = {games}/>} />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
+            </div>
+        </AuthContext.Provider>
+    );
 }
 
 export default App;
--- React Routing Workshop/router-workshop/src/components/Catalogue/Catalogue.js
+++ React Routing Workshop/router-workshop/src/components/Catalogue/Catalogue.js
@@ -1,18 +1,10 @@
-import * as gamesService from '../../services/gamesService'
-import { useEffect, useState } from "react"
 import { Game } from './Game/Game';
 
-export const Catalogue = () => {
-    const [games, setGames] = useState([]);
-
-    useEffect(() => {
-        gamesService.getAll()
-            .then(res => setGames(res));
-    }, [])
+export const Catalogue = (props) => {
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
-            {games.length > 0 ? games.map(x => <Game key={x._id} {...x} />) : <h3 className="no-articles">No articles yet</h3>}
+            {props.games.length > 0 ? props.games.map(x => <Game key={x._id} {...x} />) : <h3 className="no-articles">No articles yet</h3>}
         </section>
     )
 }
